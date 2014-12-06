59f362a759c6ed4feaae5006921727fd2d30c4c1
jung112 <jung112@purdue.edu>
2014-12-06 18:28:30 -0500

    Word Documents

--- MiniPrj_F14/Sources/main.c
+++ MiniPrj_F14/Sources/main.c
@@ -13,7 +13,7 @@
    
    - Software Leader: < John Lee >      Signature: ______________________
 
-   - Interface Leader: < Josh Wrobleski >     Signature: ______________________
+   - Interface Leader: < Josh Wrobleski >     Signature: Josh Wroblewski
 
    - Peripheral Leader: < Kyunghoon Jung >    Signature: Kyunghoon Jung
 
@@ -68,11 +68,22 @@
 char inchar(void);
 void outchar(char x);
 void setPosition(int x);
+void bco(char x);		// SCI buffered character output
+void shiftout(char);	// LCD drivers (written previously)
+void lcdwait(void);
+void send_byte(char);
+void send_i(char);
+void chgline(char);
+void print_c(char);
+void pmsglcd(char[]);
 
 
 /* Variable declarations */
 int servoPosition = 0;
 int dispenseHold = 0;
+int mmHold = 0;
+int key[4] = {0,0,0,0};
+int currentKey = 0;
    	   			 		  			 		       
 
 /* Special ASCII characters */
@@ -127,6 +138,30 @@ void  initializations(void) {
 //RTI Initialisations for 2.048 ms
   RTICTL = 0x41;
   CRGINT = CRGINT | 0x80;
+  
+//PWM initializations
+  MODRR_MODRR0 = 0x01;
+  PWME_PWME0 = 0x01;
+  PWMPOL_PPOL0 = 0x00;
+  PWMPER0 = 0xFF;
+ 	PWMDTY0 = 0x00;
+ 	PWMPRCLK = 0x70;
+ 	PWMCLK = 0x08;
+ 	PWMSCLA = 0x04;
+  
+//SPI Baud rate 6Mbs
+  DDRM   = 0xFF;
+  SPICR1 = 0x50;
+  SPICR2 = 0x00;
+  SPIBR  = 0x01;
+  
+//LCD initializations
+  PTT_PTT4 = 1;
+  PTT_PTT3 = 0;
+  send_i(LCDON);
+  send_i(TWOLINE);
+  send_i(LCDCLR);
+  lcdwait();
 
 	      
 	      
@@ -171,18 +206,108 @@ interrupt 7 void RTI_ISR(void)
     if(RTICNT > 71){
       RTICNT = 0;
       if(servoPosition == 1) {      //the pulse is ending
-        //set PWM DUTY to 0
-        //reset variable
+        //set PWM_DUTY to 0;
+<<<<<<< HEAD
+        PWMDTY0 = 0;
+=======
+        PWM_PTY0 = 0x00;
+        
+>>>>>>> origin/master
         servoPosition = 0;
       }
       
-      //Insert code for keypad
       
+      //row1 = 0;
+      //add a small delay
+      
+      /*if(!column1)
+      {
+        key[currentKey] = 1;
+      }
+      if(!column2)
+      {
+        key[currentKey] = 2;
+      }
+      if(!column3)
+      {
+        key[currentKey] = 3;
+      }
+      row1 = 1;
+      row2 = 0;
+      //delay
+      
+      if(!column1)
+      {
+        key[currentKey] = 4;
+        currentKey++;
+      }
+      if(!column2)
+      {
+        key[currentKey] = 5;
+        currentKey++;
+      }
+      if(!column3)
+      {
+        key[currentKey] = 6;
+        currentKey++;
+      }
+      row2 = 1;
+      row3 = 0;
+      //delay
+      
+      if(!column1)
+      {
+        key[currentKey] = 7;
+        currentKey++;
+      }
+      if(!column2)
+      {
+        key[currentKey] = 8;
+        currentKey++;
+      }
+      if(!column3)
+      {
+        key[currentKey] = 9;
+        currentKey++;
+      }
+      row3 = 1;
+      row4 = 0;
+      //delay
+      
+      if(!column1)
+      {
+        key[currentKey] = 10; //Star
+        currentKey = 0;
+      }
+      if(!column2)
+      {
+        key[currentKey] = 0;
+        currentKey++;
+      }
+      if(!column3)
+      {
+        key[currentKey] = 11; //hash
+        currentKey++;
+      }
+      
+      row4 = 1;
       
       
       
       //if keypad has right code 
-      //set PWM to  2ms
+      if(key[0] == 1)
+      {
+        if(key[1] == 2)
+        {
+          if(key[2] == 3)
+          {
+            if(key[3] == 4)
+            {
+              setPosition(3);  
+            }
+          }
+        }
+      } */
       //hold for 1s
       if(dispenseHold == 0 && servoPosition == 0) {
         setPosition(0); //sets position to -90;
@@ -255,15 +380,130 @@ char inchar(void) {
 void setPosition(int x) {
     //for x, 0 = -90, 1 = 0; 2 = 90;
     if(x == 0){
-     //set PWM to 1ms 
+     //set PWM to 1ms
+     PWMDTY0 = 255/10; 
     } else if (x == 1){
-     //set PWM to 1.5ms 
+     //set PWM to 1.5ms
+     PWMDTY0 = (255/10)*1.5; 
     } else {
-     //set PWM to 2ms 
+     //set PWM to 2ms
+     PWMDTY0 = 255/5; 
     }
     servoPosition = 1;
   
 }
+
+void shiftout(char x)
+
+{
+ 
+  // test the SPTEF bit: wait if 0; else, continue
+  // write data x to SPI data register
+  // wait for 30 cycles for SPI data to shift out 
+  while(SPISR_SPTEF == 0){
+  }
+  SPIDR = x;
+  lcdwait();
+
+}
+
+/*
+***********************************************************************
+  lcdwait: Delay for approx 2 ms
+***********************************************************************
+*/
+
+void lcdwait()
+{
+    int n = 8;
+    int m = 6000;
+    while(n!=0){
+      while(m){
+        m--;
+      }
+      n--;
+    }
+ 
+}
+
+/*
+*********************************************************************** 
+  send_byte: writes character x to the LCD
+***********************************************************************
+*/
+
+void send_byte(char x)
+{
+     // shift out character
+     // pulse LCD clock line low->high->low
+     // wait 2 ms for LCD to process data
+    shiftout(x);
+    PTT_PTT4 = 0;
+    PTT_PTT4 = 1;
+    PTT_PTT4 = 0;
+    lcdwait();
+}
+
+/*
+***********************************************************************
+  send_i: Sends instruction byte x to LCD  
+***********************************************************************
+*/
+
+void send_i(char x)
+{
+        // set the register select line low (instruction data)
+        // send byte
+    PTT_PTT2 = 0;
+    send_byte(x);
+}
+
+/*
+***********************************************************************
+  chgline: Move LCD cursor to position x
+  NOTE: Cursor positions are encoded in the LINE1/LINE2 variables
+***********************************************************************
+*/
+
+void chgline(char x)
+{
+    send_i(CURMOV);
+    send_i(x);
+    outchar('\r');
+    outchar('\n');
+
+}
+
+/*
+***********************************************************************
+  print_c: Print (single) character x on LCD            
+***********************************************************************
+*/
+ 
+void print_c(char x)
+{
+    PTT_PTT2 = 1;
+    send_byte(x);
+    outchar(x);
+
+}
+
+/*
+***********************************************************************
+  pmsglcd: print character string str[] on LCD
+***********************************************************************
+*/
+
+void pmsglcd(char str[])
+{
+    int i = 0;
+    while(str[i] != '\0'){
+      print_c(str[i]);
+      i++;
+    }
+
+}
+
 
 /*
 ***********************************************************************
